620823e21b686120848cecb5b7bcf4677dff087d
Andre White <adarro@gmail.com>
2020-01-08 02:39:15 -0700

    Overriding publicationRepository property to support Gradle 6.0 with shipkit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,10 @@
 plugins {
     id("org.shipkit.java") version "2.2.6"
 }
+
+// shipkit config with gradle 6
+tasks.withType(org.shipkit.gradle.notes.UpdateReleaseNotesTask::class.java) {
+    this.publicationRepository = "https://dl.bintray.com/truthencode/ddo-service"
+}
+
+// rootProject.apply { from(rootProject.file("gradle/shipkit")) }
